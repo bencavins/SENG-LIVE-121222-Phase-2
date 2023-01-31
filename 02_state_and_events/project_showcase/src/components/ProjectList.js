35799cdb886cc010b67a6b3bcdb2787a81a06eb1
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-31 10:54:42 -0500

    add filters

--- 02_state_and_events/project_showcase/src/components/ProjectList.js
+++ 02_state_and_events/project_showcase/src/components/ProjectList.js
@@ -1,8 +1,32 @@
 import ProjectListItem from "./ProjectListItem";
 
-const ProjectList = ({ projects }) => {
+import {useState} from 'react';
 
-  const projectListItems = projects.map((project) => (
+const ProjectList = ({ projects }) => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [phaseFilter, setPhaseFilter] = useState('All')
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value)
+  }
+
+  function handlePhase4Click(event) {
+    setPhaseFilter(4)
+  }
+
+  function handlePhaseAllClick(event) {
+    setPhaseFilter('All')
+  }
+
+  const filteredItems = projects.filter(project => {
+    return project.name.toLowerCase().includes(searchQuery.toLowerCase())
+  })
+  
+  const filter2 = filteredItems.filter(project => {
+    return project.phase === phaseFilter || phaseFilter === 'All'
+  })
+
+  const projectListItems = filter2.map((project) => (
     <ProjectListItem key={project.id} {...project} />
   ));
 
@@ -12,14 +36,18 @@ const ProjectList = ({ projects }) => {
       <h2>Projects</h2>
 
       <div className="filter">
-        <button>All</button>
+        <button onClick={handlePhaseAllClick}>All</button>
         <button>Phase 5</button>
-        <button>Phase 4</button>
+        <button onClick={handlePhase4Click}>Phase 4</button>
         <button>Phase 3</button>
         <button>Phase 2</button>
         <button>Phase 1</button>
       </div>
-      <input type="text" placeholder="Search..."/>
+      <p>{searchQuery}</p>
+      <input 
+        type="text" 
+        placeholder="Search..."
+        onChange={handleChange} />
 
       <ul className="cards">{projectListItems}</ul>
     </section>
