b1b44e528108e96c3066ff46b0e6aeebc9358ec0
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-02 11:12:17 -0500

    show component lifecycle

--- 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 // Deliverable 3: Demonstrate the unmounting and cleanup 
 // phase of a component through `useEffect`
 
@@ -6,3 +8,38 @@
 
     // Open up the devtools to observe when the cleanup 
     // will occur during the stages of Component Lifecycle
+
+function Timer() {
+  const [timerCount, setTimerCount] = useState(0)
+
+  useEffect(() => {
+    // setTimerCount(prevVal => prevVal + 1)
+    function doClick(event) {
+      console.log('page clicked!')
+    }
+    document.addEventListener('click', doClick)
+
+    // setInterval will continue to run outside the component lifecycle
+    const intervalId = setInterval(() => {
+      console.log('interval fired!')
+      setTimerCount(prevVal => prevVal + 1)
+    }, 1000)
+
+    console.log(`intervalId=${intervalId}`)
+
+    // Return a function that will clean up the interval behavior
+    return () => {
+      clearInterval(intervalId)
+      console.log('interval cleared!')
+
+      document.removeEventListener('click', doClick)
+      console.log('click event removed!')
+    }
+  }, [])
+
+  return (
+    <h2>Timer: {timerCount}</h2>
+  )
+}
+
+export default Timer;
