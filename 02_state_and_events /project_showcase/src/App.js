2a0447745b14b991c7bd49dc0b199d5d57da3468
Ben Cavins <ben.cavins@flatironschool.com>
2023-05-23 16:03:41 -0400

    lec

--- 02_state_and_events /project_showcase/src/App.js
+++ 02_state_and_events /project_showcase/src/App.js
@@ -4,10 +4,24 @@ import ProjectList from "./components/ProjectList";
 
 import projects from "./projects";
 
+import { useState } from "react"
+
 const App = () => {
+
+  // [stateVariable, stateSetterFunction]
+  const [isDarkMode, setIsDarkMode] = useState(true)
+
+  function handleDarkMode(event) {
+    setIsDarkMode(prevVal => !prevVal)
+    // f = (prevVal) => !prevVal
+    // isDarkMode = f(isDarkMode)
+    // isDarkMode = f(true)
+    // isDarkMode = false
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header handleClick={handleDarkMode} isDarkMode={isDarkMode} />
       <ProjectForm />
       <ProjectList projects={projects} />
     </div>
--- 02_state_and_events /project_showcase/src/components/ProjectList.js
+++ 02_state_and_events /project_showcase/src/components/ProjectList.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 import ProjectListItem from "./ProjectListItem";
 
 const ProjectList = ({ projects }) => {
 
-  const projectListItems = projects.map((project) => (
+  const [search, setSearch] = useState("")
+  const [phase, setPhase] = useState("All")
+
+  function handleChange(event) {
+    setSearch(event.target.value)
+  }
+
+  function handleP3() {
+    setPhase(3)
+  }
+
+  const filteredPhaseProjects = projects.filter(project => {
+    return project.phase === phase || phase === "All"
+  })
+
+  const filteredProjects = filteredPhaseProjects.filter((project) => {
+    return project.name.includes(search.toLowerCase())
+  })
+
+  const projectListItems = filteredProjects.map((project) => (
+    // <ProjectListItem name={project.name} about={project.about} />
     <ProjectListItem key={project.id} {...project} />
   ));
+
 
 
   return (
@@ -15,11 +38,14 @@ const ProjectList = ({ projects }) => {
         <button>All</button>
         <button>Phase 5</button>
         <button>Phase 4</button>
-        <button>Phase 3</button>
+        <button onClick={handleP3}>Phase 3</button>
         <button>Phase 2</button>
         <button>Phase 1</button>
       </div>
-      <input type="text" placeholder="Search..."/>
+      <input 
+        type="text" 
+        placeholder="Search...!!!!!!"
+        onChange={handleChange} />
 
       <ul className="cards">{projectListItems}</ul>
     </section>
